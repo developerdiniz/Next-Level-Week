d4e5f6fa5fb754b5c063c0332519892099b6cd3e
developerdiniz <mvl.rafael6@gmail.com>
2020-06-04 22:20:10 -0300

    Olhando as oportunidades.

---
+++ Scripts/Introducao-js.js
@@ -0,0 +1,40 @@
+//objeto: possuem propriedades e funcionalidade
+
+//const pessoa = {
+//  altura: "1,80m",
+//  idade: 28,
+//  solteiro: false,
+//  correr(){
+//    return "Run Diniz"
+//  }
+//}
+//document.write(pessoa.correr());
+
+//Array -Vetores
+//const colecao = ['Blue','Green','Red', 1,{name:'Myname'}]
+//document.write(colecao[4].name)
+
+//Funções - Funcionalidades - Atalhos - Reuso de código.
+//function sayMyName(name){
+//  document.write(name)
+//}
+//sayMyName("Natany")
+//sayMyName("&")
+//sayMyName("Rafael")
+
+//Condicionais
+
+//function mediafinal(notafinal){
+//  if (notafinal >= 7) {
+//    document.write("Aprovado")
+//  }else {
+//    document.write("Reprovado")
+//  }
+//}
+//mediafinal(7)
+
+//Laço de repetição
+//for (i = 0; i <= 10; i++){
+//  document.write(i)
+//}
+

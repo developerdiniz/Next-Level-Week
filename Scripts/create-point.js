7999d9c71fcbf437fcfe44d7c0e6fa96163a4281
developerdiniz <mvl.rafael6@gmail.com>
2020-06-05 00:47:43 -0300

    Olhando as oportunidades.

--- Scripts/create-point.js
+++ Scripts/create-point.js
@@ -7,9 +7,9 @@ function populateUFs(){
     //Variavel que referencia o select uf
     const ufSelect = document.querySelector("select[name=uf]")
 
-    const urlUF = `https://servicodados.ibge.gov.br/api/v1/localidades/estados`
+     
 
-    fetch(urlUF)
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
     .then( res => res.json() ) //funçao anonima que esta retornando um valor
     .then ( states => {
         for (const state of states){
@@ -27,26 +27,21 @@ function getCities(event){
 
     const ufValue = event.target.value
     
-    const urlCity = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
-    fetch(urlCity)
-    .then(res => res.json())
-    .then(cities => {
-        for (const city of cities) {
+    fetch(url)
+    .then( res => res.json() )
+    .then( cities => {
+        for( const city of cities) {
             citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
         }
         citySelect.disabled = false
     })
 }
 
-
-
-
-
-
 //Procurar pelo select para trabalhar os dados.
 document
-    .querySelectorAll("select[name=uf]") 
-    .addEventListener('change', getCities)
+    .querySelector("select[name=uf]") 
+    .addEventListener("change", getCities)
     //ouvidor de envento
     //() => {} == Criar uma função anonima 
